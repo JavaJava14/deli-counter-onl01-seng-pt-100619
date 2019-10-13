9008080d6a1c24b3dcc75e478806fffdcd744ec1
Jose Chavira <joeychavira2016@gmail.com>
2019-10-13 04:17:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,9 @@
 
 def line(katz_deli)
   if katz_deli.length == 0
-  puts "The line is currently empty."
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
   end
 end
 
