906b28415839af7e1fb3e19dc3d71691eaf39252
Jose Chavira <joeychavira2016@gmail.com>
2019-10-13 04:56:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,5 +13,5 @@ end
 
 def take_a_number(deli,name)
   deli.push(name)
-  puts "Welcome, #{name}. You are number #{deli.length} in the line."
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
 end
