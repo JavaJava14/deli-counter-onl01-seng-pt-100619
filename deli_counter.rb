a36a5905335229d90a4ab7af549a980d1156522f
Jose Chavira <joeychavira2016@gmail.com>
2019-10-13 04:59:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,3 +15,9 @@ def take_a_number(deli,name)
   deli.push(name)
   puts "Welcome, #{name}. You are number #{deli.length} in line."
 end
+
+def now_serving(deli)
+  if deli.length == 0
+    puts "There is nobody waiting to be served!"
+  end
+end
